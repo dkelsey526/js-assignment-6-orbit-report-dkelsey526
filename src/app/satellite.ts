fb9af2c9f1804c9a8d16b6d09119cc09ebd0f9ed
dkelsey526 <dkelsey@dkcreativeart.com>
2022-08-29 21:27:39 -0500

    assign6step3

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -5,19 +5,33 @@ export class Satellite {
 	launchDate: string;
 	orbitType: string;
 	operational: boolean;
+	
 
-	constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+	constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean,) {
 		this.name = name;
 		this.type = type;
 		this.launchDate = launchDate;
 		this.orbitType = orbitType;
 		this.operational = operational;
+		this.isSpaceDebris();
    }
 	
 	isSpaceDebris(): boolean {
+		if (this.type === 'Space Debris') {
 		return true;
-   }
+ 	} else {
+		 	return false;
+ 	
+	}
+}
+// TODO 3a: fix isSpaceDebris check
+// isSpaceDebris returns true if the satellite type is 'Space Debris', and it returns false otherwise.
+
+// <div *ngIf="isSpaceDebris">Space Debris </div> 
+
+// isSpaceDebris(): boolean {
+
+// 	return true;
+// }
 
 }
-
-// TODO 3a: fix isSpaceDebris check
